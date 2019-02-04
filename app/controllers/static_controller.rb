47f9eb57ab88f5e44ebeada881c8848797a4a7b9
LordGoblin <maxime78600@gmail.com>
2019-02-04 16:28:16 +0100

    ok

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -8,4 +8,12 @@ def index_contact
 	puts "mdr"*50
 end
 
+def index_welcome
+	@user_welcome = params["name"]
+end
+
+def index_accueil
+	puts "lol"*50
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/team', to: 'static#index_team'
   get '/contact', to: 'static#index_contact'
+  get '/welcome/:name', to: 'static#index_welcome'
+  get '/accueil', to: 'static#index_accueil'
 end
